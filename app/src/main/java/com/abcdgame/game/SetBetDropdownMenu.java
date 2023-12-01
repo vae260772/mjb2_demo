115523f083feb25a836fd77351d502a67fe39ef8
lihui <973742475@qq.com>
2023-12-01 17:08:56 +0800

    66666666

---
+++ app/src/main/java/com/abcdgame/game/SetBetDropdownMenu.java
@@ -0,0 +1,40 @@
+package com.abcdgame.game;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.PopupWindow;
+
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SetBetDropdownMenu extends PopupWindow {
+    private Context context;
+    private RecyclerView rvCategory;
+    private SetBetDropdownAdapter dropdownAdapter;
+
+    public SetBetDropdownMenu(Context context){
+        super(context);
+        this.context = context;
+        setupView();
+    }
+
+    public void setBetSelectedListener(SetBetDropdownAdapter.BetSelectedListener betSelectedListener) {
+        dropdownAdapter.setBetSelectedListener(betSelectedListener);
+    }
+
+    private void setupView() {
+        View view = LayoutInflater.from(context).inflate(R.layout.popout_category, null);
+
+        rvCategory = view.findViewById(R.id.rvBets);
+        rvCategory.setHasFixedSize(true);
+        rvCategory.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
+        rvCategory.addItemDecoration(new DividerItemDecoration(context, LinearLayoutManager.VERTICAL));
+
+        dropdownAdapter = new SetBetDropdownAdapter(SetBet.generateBetsList());
+        rvCategory.setAdapter(dropdownAdapter);
+
+        setContentView(view);
+    }
+}
---
+++ app/src/main/java/com/abcdgame/game/SettingsActivity.java
@@ -0,0 +1,65 @@
+package com.abcdgame.game;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+public class SettingsActivity extends AppCompatActivity {
+
+
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            Preference button = (Preference) getPreferenceManager().findPreference("balanceBtn");
+            if (button != null) {
+                button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference arg0) {
+                        Toast.makeText(getActivity(), "$1000 added to your balance",
+                                Toast.LENGTH_SHORT).show();
+
+                        SharedPreferences mSharedPrefs;
+                        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                        int currentBalance = 0;
+                        currentBalance = mSharedPrefs.getInt("balance", currentBalance);
+                        SharedPreferences.Editor editor = mSharedPrefs.edit();
+                        currentBalance += 1000;
+                        editor.putInt("balance", currentBalance);
+                        editor.commit();
+                        return true;
+                    }
+                });
+
+            }
+        }
+    }
+
+
+}
